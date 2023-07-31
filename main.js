18fef8e5ab3f586c569c73b1039d48f1c2be0cd1
PatrickCodeing <117385304+PatrickCodeing@users.noreply.github.com>
2023-07-31 17:38:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+disney = "";
+peter_pan = "";
+
+function preload(){
+    disney = loadSound("Disney_music.mp3");
+    peter_pan = loadSound("peter_pan.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
