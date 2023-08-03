f40c4be51b85607d67cc93d196dfe71b344330e2
PatrickCodeing <117385304+PatrickCodeing@users.noreply.github.com>
2023-08-03 12:29:31 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 disney = "";
 peter_pan = "";
+leftX = 0;
+leftY = 0;
+rightX = 0;
+rightY = 0;
 
 function preload(){
     disney = loadSound("Disney_music.mp3");
@@ -12,8 +16,29 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PozeNet Is Initialized");
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftX = results[0].pose.leftWrist.x;
+        leftY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " +leftX+"leftWristY = "+leftY);
+
+        rightX = results[0].pose.rightWrist.x;
+        rightY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " +rightX+"rightWristY = "+rightY);
+    }
 }
