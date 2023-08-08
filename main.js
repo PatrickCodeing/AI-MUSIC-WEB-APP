9fcb430a0ae5f16bdb3466d748975bbe2209690e
PatrickCodeing <117385304+PatrickCodeing@users.noreply.github.com>
2023-08-08 17:04:25 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ leftX = 0;
 leftY = 0;
 rightX = 0;
 rightY = 0;
-
+status2 = "";
 function preload(){
     disney = loadSound("Disney_music.mp3");
     peter_pan = loadSound("peter_pan.mp3");
@@ -27,18 +27,33 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    status1 = disney.isPlaying();
+
+    fill("red");
+    stroke("red");
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftX, leftY, 20);
+        
+        status2.stop();
+    if(status1 = false){
+        status1.play();
+        document.getElementById("song").innerHTML = "Disney";
+    }
+     }
 }
 
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
         leftX = results[0].pose.leftWrist.x;
         leftY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " +leftX+"leftWristY = "+leftY);
+        console.log("leftWristX = " +leftX+" leftWristY = "+leftY);
 
         rightX = results[0].pose.rightWrist.x;
         rightY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " +rightX+"rightWristY = "+rightY);
+        console.log("rightWristX = " +rightX+" rightWristY = "+rightY);
     }
 }
